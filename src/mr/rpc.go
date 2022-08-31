422ddd5da826721c28bd2e3c4b599ba14270559a
gotgotaind <gotgotaind@gmail.com>
2022-08-31 19:13:13 +0200

    with a getreply struct in rpc

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -23,7 +25,12 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
-
+type GetWorkReply struct {
+	MapOrReduce string
+	Filename    string
+	Nreduce     int
+	ReduceId    int
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
